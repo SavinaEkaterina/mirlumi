203f58c5daa592d2a2b7ddd588ee15a37bf0bced
Екатерина Савина <27101986er@gmail.com>
2026-09-07 10:00:26 +0300

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,10 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig} from 'vite';
-
 export default defineConfig(() => {
-  return {
-    plugins: [react(), tailwindcss()],
+ return {
+ base: '/mirlumi/',
+  plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
